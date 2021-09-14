014d82a292da8702e0a2c70c13d549e5a339f86e
Ayush Tayal <ayushtayal1111@gmail.com>
2021-09-14 14:59:18 +0530

    calculator is made

---
+++ cal2.js
@@ -0,0 +1,28 @@
+const buttons = document.querySelectorAll(".uinput");
+const input = document.querySelector(".input");
+const clear = document.querySelector(".clear");
+const equal = document.querySelector(".equal");
+const backspace = document.querySelector(".backspace");
+// console.log(backspace)
+// console.log(equal)
+// console.log(buttons, input);
+
+const updateInput = (e)=>{
+    input.value += e.target.innerText;
+}
+
+buttons.forEach(fun=>{
+    fun.addEventListener("click", updateInput)
+})
+
+clear.addEventListener("click", ()=>input.value="");
+
+equal.addEventListener("click", ()=>{
+    const ans = input.value.replace("^", "**");
+    input.value = eval(ans);    
+})
+
+backspace.addEventListener("click", ()=>{
+    input.value = input.value.slice(0, length-1);
+})
+
